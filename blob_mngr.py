3b8000bfa82f6673d45858ce84a0ab9b3c98f088
Dmitry Mikhaylov <dmik@vt.edu>
2020-12-15 16:48:51 -0500

    azure blom manager added

---
+++ blob_mngr.py
@@ -0,0 +1,8 @@
+from azure.storage.blob import BlobServiceClient
+
+blob_container = app.config['BLOB_CONTAINER']
+storage_url = "https://{}.blob.core.windows.net/".format(app.config['BLOB_ACCOUNT'])
+blob_service = BlobServiceClient(account_url=storage_url, credential=app.config['BLOB_STORAGE_KEY'])
+
+blob_client = blob_service.get_blob_client(container=blob_container, blob=filename)
+blob_client.upload_blob(file)
